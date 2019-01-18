ef6bcc808a7ec114ce1843ded084278cdd1397d6
levyrafi18 <raffilevy@gmail.com>
2019-01-18 23:11:20 +0200

    update

--- ex4/backprop_main.py
+++ ex4/backprop_main.py
@@ -5,8 +5,7 @@ import skeleton_backprop_network
 
 
 training_data, test_data = backprop_data.load(train_size=10000,test_size=5000)
-
-net = skeleton_backprop_network.Network([784, 40, 10])
-net.network_output_before_softmax(training_data[0][0])
-
+# net = skeleton_backprop_network.Network([784, 40, 10])
 # net.SGD(training_data, epochs=30, mini_batch_size=10, learning_rate=0.1, test_data=test_data)
+net = skeleton_backprop_network.Network([784, 30, 30, 30, 30, 10])
+net.SGD(training_data, epochs=30, mini_batch_size=10000, learning_rate=0.1, test_data=test_data)
