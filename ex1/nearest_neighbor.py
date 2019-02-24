8937ccbbf1a448646dba33f05d188e7d955f17b6
levyrafi12 <raffilevy@gmail.com>
2019-02-24 20:47:26 +0200

    update

--- ex1/nearest_neighbor.py
+++ ex1/nearest_neighbor.py
@@ -30,26 +30,30 @@ test_labels = labels[idx[10000:]]
 # L2 metric. In case of a tie between the k labels of neighbors, it will choose an arbitrary
 # option.
 
-def knn(mat, labels, vec, k):
-	# euclid_mat = np.apply_along_axis(lambda x: np.sqrt(np.dot((x - vec),(x - vec))), 1, mat)
-	euclid_mat = np.sum((mat - vec) * (mat - vec), axis=1) # sum is performed along each row
-	indices = np.argsort(euclid_mat)
-	nearest_idx = indices[:k]
-	nearest_labels = [labels[i] for i in nearest_idx]
+def knn(mat, labels, vecs, k):
+	n = vecs.shape[0] # num test images
+	pred_labels = np.zeros(n)
+	for i in range(n):
+		vec = vecs[i]
+		# euclid_mat = np.apply_along_axis(lambda x: np.sqrt(np.dot((x - vec),(x - vec))), 1, mat)
+		euclid_mat = np.sum((mat - vec) * (mat - vec), axis=1) # sum is performed along each row
+		indices = np.argsort(euclid_mat)
+		nearest_idx = indices[:k]
+		nearest_labels = [labels[i] for i in nearest_idx]
 
-	c = Counter(nearest_labels) # a dictionary of keys and their counts
-	return c.most_common(1)[0][0]
+		c = Counter(nearest_labels) # a dictionary of keys and their counts
+		pred_labels[i] = c.most_common(1)[0][0]
+
+	return pred_labels
 
 def calc_loss_given_k(X, y, k):
 	loss = 0
-	n = len(test[:100])
 	neigh = KNeighborsClassifier(n_neighbors=k)
 	neigh.fit(X, y)
-	for i in range(n):
-		# pred_label = knn(X, y, test[i], k)
-		pred_label = neigh.predict(test[i])[0]
-		loss += (pred_label != test_labels[i])
-	return loss / n
+	# pred_labels = knn(X, y, test, k)
+	pred_labels = neigh.predict(test)
+	loss += np.sum(pred_labels != test_labels)
+	return loss / test.shape[0]
 
 def visualize_data(X, y):
 	pca = PCA(n_components=2)
@@ -57,12 +61,14 @@ def visualize_data(X, y):
 
 	plt.scatter(x=pca_result[:, 0], y = pca_result[:, 1], c=y, cmap=plt.cm.get_cmap('Paired'))
 	plt.show()
+	plt.close()
 
 def iterate_over_k(from_k, to_k, step):
 	acc_vals = []
 	k_vals = []
 
 	for k in range(from_k, to_k + step):
+		# print("k {}".format(k))
 		loss = calc_loss_given_k(train[:1000], train_labels[:1000], k)
 		acc_vals.append(1 - loss)
 		k_vals.append(k)
@@ -70,21 +76,23 @@ def iterate_over_k(from_k, to_k, step):
 	best_acc, best_k = sorted(zip(acc_vals, k_vals), reverse=True)[0]
 	print("Best K is {} with accuracy {}".format(best_k, best_acc))
 
-	title = 'acc_as_func_of_k'
+	title = 'MNIST - K Nearest Neighbor'
 	plt.text(0.5, 0.9, 'accuracy as a functioben of k', transform=plt.gca().transAxes, ha='center')
 	plt.title(title)
 	plt.plot(k_vals, acc_vals, 'r-')
-	plt.ylim(ymax = 1)
+	plt.ylim(ymax = max(acc_vals))
 	plt.ylabel('Accuracy')
-	plt.xlabel('k')
+	plt.xlabel('k-NN')
 	plt.savefig(title)
 	# plt.show()
+	plt.close()
 
 def iterate_over_n(from_n, to_n, step):
 	acc_vals = []
 	n_vals = []
 	
 	for n in range(from_n, to_n + step, step):
+		# print("n {}".format(n))
 		loss = calc_loss_given_k(train[:n], train_labels[:n], 1)
 		acc_vals.append(1 - loss)
 		n_vals.append(n)
@@ -92,15 +100,16 @@ def iterate_over_n(from_n, to_n, step):
 	best_acc, best_n = sorted(zip(acc_vals, n_vals), reverse=True)[0]
 	print("Best n is {} with accuracy {}".format(best_n, best_acc))
 
-	title = 'acc_as_func_of_n'
-	plt.text(0.5, 0.9, 'accuracy as a function of n', transform=plt.gca().transAxes, ha='center')
+	title = 'MNIST - 1 Nearest Neighbor'
+	plt.text(0.5, 0.5, 'accuracy as a function of n', transform=plt.gca().transAxes, ha='center')
 	plt.title(title)
 	plt.plot(n_vals, acc_vals, 'b-')
-	plt.ylim(ymax = 1)
+	plt.ylim(ymax = max(acc_vals))
 	plt.ylabel('Accuracy')
-	plt.xlabel('n')
+	plt.xlabel('samples (n)')
 	plt.savefig(title)
 	# plt.show()
+	plt.close()
 
 if __name__ == '__main__':
 	# loss = calc_loss_given_k(10)
