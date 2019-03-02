7b76858ca4adaa74c26eaf07a4df9cf090a716b2
levyrafi18 <raffilevy@gmail.com>
2019-03-02 20:28:52 +0200

    update

--- ex2/skeleton.py
+++ ex2/skeleton.py
@@ -9,14 +9,12 @@ from intervals import find_best_interval
 
 class Assignment2(object):
 	"""Assignment 2 skeleton.
-
 	Please use these function signatures for this assignment and submit this file, together with the intervals.py.
 	"""
 
 	def sample_from_D(self, m):
 		"""Sample m data samples from D.
 		Input: m - an integer, the size of the data sample.
-
 		Returns: np.ndarray of shape (m,2) :
 			A two dimensional array of size m that contains the pairs where drawn from the distribution P.
         """
@@ -34,17 +32,14 @@ class Assignment2(object):
 		Plots the data as asked in (a) i ii and iii.
 		Input: m - an integer, the size of the data sample.
 			k - an integer, the maximum number of intervals.
-
 		Returns: None.
 		"""
 
 		S = self.sample_from_D(m)
 
-		intervals, besterror = find_best_interval(S[0,:], S[1,:] ,k)
+		intervals, _ = find_best_interval(S[0,:], S[1,:] ,k)
 		for inter in intervals:
-			plt.hlines(0.8, inter[0], inter[1], 'b', lw=2) # print horizontal line
-        
-		print("besterror = {}".format(besterror))
+			plt.hlines(0.8, inter[0], inter[1], 'b', lw=3) # print horizontal line
 
 		plt.plot(S[0,:], S[1, :], 'ro')
 
@@ -56,8 +51,8 @@ class Assignment2(object):
 		plt.axis([0, 1, -0.1, 1.1])
 		plt.title(title)
 		plt.savefig(title + '.png')
-		plt.close()
 		# plt.show()
+		plt.close()
 		return
 
 	def experiment_m_range_erm(self, m_first, m_last, step, k, T):
@@ -69,7 +64,6 @@ class Assignment2(object):
 			   step - an integer, the difference between the size of m in each loop.
 			   k - an integer, the maximum number of intervals.
 			   T - an integer, the number of times the experiment is performed.
-
 		Returns: np.ndarray of shape (n_steps,2).
 			A two dimensional array that contains the average empirical error
 			and the average true error for each m in the range accordingly.
@@ -94,17 +88,17 @@ class Assignment2(object):
 
 		m_vals = np.arange(m_first, m_last + step, step)
     
-		plt.plot(m_vals, E[:, 0], 'r-', m_vals, E[:, 1], 'b--')
-		plt.axis([m_first, m_last, 0, 1])
-		plt.text(20, 0.8, 'red = Es')
-		plt.text(20, 0.7, 'blue = Ep')
+		plt.plot(m_vals, E[:, 0], 'r-', m_vals, E[:, 1], 'b-')
+		plt.axis([m_first, m_last, 0, E.max()])
+		plt.text(0.5, 0.9, 'red = Es', transform=plt.gca().transAxes, ha='center')
+		plt.text(0.5, 0.85, 'blue = Ep', transform=plt.gca().transAxes, ha='center')
 		title = 'm_range_erm'
-		plt.xlabel('m')
+		plt.xlabel('samples (m)')
 		plt.ylabel('Error')
 		plt.title(title)
 		plt.savefig(title + '.png')
-		plt.close()
 		# plt.show()
+		plt.close()
 
 		return E
 
@@ -115,7 +109,6 @@ class Assignment2(object):
 			   k_first - an integer, the maximum number of intervals in the first experiment.
 			   m_last - an integer, the maximum number of intervals in the last experiment.
 			   step - an integer, the difference between the size of k in each experiment.
-
 		Returns: The best k value (an integer) according to the ERM algorithm.
 		"""
 		n_steps = int((k_last - k_first) / step + 1)
@@ -133,17 +126,17 @@ class Assignment2(object):
         
 		k_vals = np.arange(k_first, k_last + step, step)
     
-		plt.plot(k_vals, E[:,0], 'r-', k_vals, E[:,1], 'b--')
-		plt.axis([k_first, k_last, 0, 1])
-		plt.text(2, 0.8, 'red = Es')
-		plt.text(2, 0.7, 'blue = Ep')
+		plt.plot(k_vals, E[:,0], 'r-', k_vals, E[:,1], 'b-')
+		plt.axis([k_first, k_last, E.min(), E.max()])
+		plt.text(0.5, 0.9, 'red = Es', transform=plt.gca().transAxes, ha='center')
+		plt.text(0.5, 0.85, 'blue = Ep', transform=plt.gca().transAxes, ha='center')
 		title = 'k_range_erm'
 		plt.xlabel('k')
 		plt.ylabel('Error')
 		plt.title(title)
 		plt.savefig(title + '.png')
-		plt.close()
 		# plt.show()
+		plt.close()
 
 		return np.argmin(E[:,0])
 
@@ -155,11 +148,10 @@ class Assignment2(object):
 			   k_first - an integer, the maximum number of intervals in the first experiment.
 			   k_last - an integer, the maximum number of intervals in the last experiment.
 			   step - an integer, the difference between the size of k in each experiment.
-
 		Returns: The best k value (an integer) according to the SRM algorithm.
 		"""
-		n_steps = int((k_last - k_first) / step + 1)
-		E = np.zeros((n_steps, 3), dtype=float)
+		num_k = int((k_last - k_first) / step + 1)
+		E = np.zeros((num_k, 4), dtype=float)
 
 		S = self.sample_from_D(m)
 		i = 0
@@ -172,24 +164,25 @@ class Assignment2(object):
 			i += 1
 		
 		k_vals = np.arange(k_first, k_last + step, step)
-		sum_error = E[:,0] + E[:,2]
+		E[:,3] = E[:,0] + E[:,2]
 
 		plt.plot(k_vals, E[:,0], 'r-', k_vals, E[:,1], 'b-', k_vals, E[:,2], 'g-', \
-			k_vals, sum_error, 'y-')
+			k_vals, E[:,3], 'y-')
 
-		plt.axis([k_first, k_last, 0, 1])
-		plt.text(2, 0.8, 'red = Es')
-		plt.text(2, 0.7, 'blue = Ep')
-		plt.text(2, 0.6, 'green = PenaltySrm')
-		plt.text(2, 0.5, 'yellow = Es + PenaltySrm')
-		plt.xlabel('k')
+		plt.axis([k_first, k_last, E.min(), E.max()])
+		plt.text(0.8, 0.6, 'red = Es', transform=plt.gca().transAxes, ha='center')
+		plt.text(0.8, 0.55, 'blue = Ep', transform=plt.gca().transAxes, ha='center')
+		plt.text(0.8, 0.5, 'green = PenaltySrm', transform=plt.gca().transAxes, ha='center')
+		plt.text(0.8, 0.45, 'yellow = Es + PenaltySrm', transform=plt.gca().transAxes, ha='center')
+		plt.xlabel('num intervals (K)')
 		plt.ylabel('Error')
 		title = 'k_range_srm'
 		plt.title(title)
 		plt.savefig(title + '.png')
 		# plt.show()
+		plt.close()
 
-		best_k = np.argmin(sum_error)
+		best_k = np.argmin(E[:,3])
 		print("best k value found by srm {}".format(best_k + 1))
 
 		return best_k
@@ -199,7 +192,6 @@ class Assignment2(object):
 		Chooses the best hypothesis based on 3 experiments.
 		Input: m - an integer, the size of the data sample.
 			   T - an integer, the number of times the experiment is performed.
-
 		Returns: The best k value (an integer) found by the cross validation algorithm.
 		"""
 		n_steps = int((k_last - k_first) / step + 1)
@@ -260,7 +252,6 @@ class Assignment2(object):
 		"""
 		Caclulate the true error (Ep(h))
 		Input: h - the hypothesis
-
 		Returns: Ep(h)
 		"""
 
@@ -316,15 +307,15 @@ class Assignment2(object):
 		"""
 
 		# vcdim(H_k) = 2k
-		return (1 / m) * (2 * k * np.log(m / (2 * k)) + np.log(1 / delta))
+		return np.sqrt((1 / m) * (2 * k * np.log(m / (2 * k)) + np.log(1 / delta)))
 
 	#################################
 
 
 if __name__ == '__main__':
 	ass = Assignment2()
-	# ass.draw_sample_intervals(100, 3) # m = 100, k = 3 
-	#ass.experiment_m_range_erm(10, 100, 5, 3, 100) 
-	# ass.experiment_k_range_erm(1500, 1, 10, 1)
-	# ass.experiment_k_range_srm(1500, 1, 10, 1)
+	ass.draw_sample_intervals(100, 3) # m = 100, k = 3 
+	ass.experiment_m_range_erm(10, 100, 5, 3, 100) 
+	ass.experiment_k_range_erm(1500, 1, 10, 1)
+	ass.experiment_k_range_srm(1500, 1, 10, 1)
 	ass.cross_validation(1500, 1, 10, 1, 3)
