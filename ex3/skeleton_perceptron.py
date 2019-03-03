4a44646ab1c3d02cf56b933eadd3c7f7ef40f2cc
levyrafi18 <raffilevy@gmail.com>
2019-03-03 19:42:35 +0200

    update

--- ex3/skeleton_perceptron.py
+++ ex3/skeleton_perceptron.py
@@ -6,7 +6,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from skeleton_sgd import helper
+from skeleton_sgd import helper_unscale
 from skeleton_sgd import score
 
 """
@@ -16,7 +16,7 @@ Please use the provided function signature for the perceptron implementation.
 Feel free to add functions and other code, and submit this file with the name perceptron.py
 """
 
-def perceptron(data, labels, T):
+def perceptron(data, labels, t=0):
 	"""
 	returns: nd array of shape (data.shape[1],) or (data.shape[1],1) representing the perceptron classifier
 	"""
@@ -24,13 +24,43 @@ def perceptron(data, labels, T):
 	d = data.shape[1]
 	w = np.zeros(d)
 
-	for t in range(T):
-		idx = np.random.RandomState(t).permutation(n)
-		for j in idx:
-			y_pred = np.sign(np.dot(w, data[j]) * 2 - 1)
-			if y_pred != labels[j]:
-				w += labels[j] * data[j]	
+	# clf = Perceptron(random_state=t)
+	# clf.fit(data, labels)
+
+	idx = np.random.RandomState(t).permutation(n)
+	for j in idx:
+		y_pred = np.sign(np.dot(w, data[j, :]) * 2 - 1)
+		if y_pred != labels[j]:
+			w += labels[j] * data[j, :]	
 	return w
+
+def iter_multiple_samples(train_data, train_labels, test_data, test_labels):
+	multiple_samples = [5,50,100,500,1000,5000]
+	T = 100
+
+	for n in multiple_samples:
+		acc = np.zeros(T)
+		for t in range(T):
+			w = perceptron(train_data[:n,:], train_labels[:n], t)
+			acc[t] = score(w, test_data, test_labels)
+
+		acc = np.sort(acc)
+	
+		print("average test accuracy {}, n {}".format(np.sum(acc) / T, n))
+		print("5% of test accuracy {}, n {}".format(acc[5], n))
+		print("95% of test accuracy {}, n {}".format(acc[95], n))
+
+def normalize(mat):
+	return np.apply_along_axis(lambda x: x / np.linalg.norm(x), 1,mat)
+
+def helper_normalize():
+	train_data, train_labels, validation_data, validation_labels, test_data, test_labels = helper_unscale()
+	train_data_normalized = normalize(train_data)
+	validation_data_normalized = normalize(validation_data)
+	test_data_normalized = normalize(test_data)
+
+	return train_data_normalized, train_labels, validation_data_normalized, \
+		validation_labels, test_data_normalized, test_labels
 
 #################################
 
@@ -39,15 +69,9 @@ def perceptron(data, labels, T):
 #################################
 
 if __name__ == '__main__':
-	train_data, train_labels, validation_data, validation_labels, test_data, test_labels = helper()
+	train_data, train_labels, validation_data, validation_labels, test_data, test_labels = helper_normalize()
+	iter_multiple_samples(train_data, train_labels, test_data, test_labels)
 
-	w = perceptron(train_data, train_labels, 50)
-	acc = score(w, train_data, train_labels)
-	print("train accuracy {}".format(acc))
-
-	acc = score(w, validation_data, validation_labels)
-	print("validation accuracy {}".format(acc))
-
-	acc = score(w, test_data, test_labels)
-	print("test accuracy {}".format(acc))
-
+	w = perceptron(train_data, train_labels)
+	plt.imshow(np.reshape(w, (28, 28)), interpolation='nearest')
+	plt.show()
