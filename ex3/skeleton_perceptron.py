961a126bc6d44023a2828704cfd8696d6e7d5f14
levyrafi18 <raffilevy@gmail.com>
2019-03-06 19:49:54 +0200

    update

--- ex3/skeleton_perceptron.py
+++ ex3/skeleton_perceptron.py
@@ -8,6 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from skeleton_sgd import helper_unscale
 from skeleton_sgd import score
+from texttable import Texttable
 
 """
 Assignment 3 question 1 skeleton.
@@ -37,6 +38,7 @@ def perceptron(data, labels, t=0):
 def iter_multiple_samples(train_data, train_labels, test_data, test_labels):
 	multiple_samples = [5,50,100,500,1000,5000]
 	T = 100
+	table_rows = [['n','avg','5%','95%']]
 
 	for n in multiple_samples:
 		acc = np.zeros(T)
@@ -45,22 +47,40 @@ def iter_multiple_samples(train_data, train_labels, test_data, test_labels):
 			acc[t] = score(w, test_data, test_labels)
 
 		acc = np.sort(acc)
-	
-		print("average test accuracy {}, n {}".format(np.sum(acc) / T, n))
-		print("5% of test accuracy {}, n {}".format(acc[5], n))
-		print("95% of test accuracy {}, n {}".format(acc[95], n))
+		table_rows.append([n, np.sum(acc) / T, acc[5], acc[95]])
+
+	t = Texttable()
+	t.add_rows(table_rows)
+	print(t.draw())
 
 def normalize(mat):
 	return np.apply_along_axis(lambda x: x / np.linalg.norm(x), 1,mat)
 
 def helper_normalize():
 	train_data, train_labels, validation_data, validation_labels, test_data, test_labels = helper_unscale()
-	train_data_normalized = normalize(train_data)
-	validation_data_normalized = normalize(validation_data)
-	test_data_normalized = normalize(test_data)
+	train_data_norm = normalize(train_data)
+	validation_data_norm = normalize(validation_data)
+	test_data_norm = normalize(test_data)
 
-	return train_data_normalized, train_labels, validation_data_normalized, \
-		validation_labels, test_data_normalized, test_labels
+	return train_data_norm, train_labels, validation_data_norm, validation_labels, \
+		test_data_norm, test_labels
+
+def plot_misclassified_test(w):
+	n = len(test_labels)
+	idx = []
+
+	for i in range(n):
+		if np.sign((np.matmul(test_data[i], w.T) * 2 - 1)) != test_labels[i]:
+			idx.append(i)
+
+	print("misclassified test labels {}".format(test_labels[idx[0]]))
+	plt.imshow(np.reshape(test_data[idx[0]], (28, 28)), interpolation='nearest')
+	plt.show()
+	plt.close()
+
+	print("misclassified test labels {}".format(test_labels[idx[2]]))
+	plt.imshow(np.reshape(test_data[idx[2]], (28, 28)), interpolation='nearest')
+	plt.show()
 
 #################################
 
@@ -70,8 +90,12 @@ def helper_normalize():
 
 if __name__ == '__main__':
 	train_data, train_labels, validation_data, validation_labels, test_data, test_labels = helper_normalize()
-	iter_multiple_samples(train_data, train_labels, test_data, test_labels)
+	# iter_multiple_samples(train_data, train_labels, test_data, test_labels)
 
 	w = perceptron(train_data, train_labels)
-	plt.imshow(np.reshape(w, (28, 28)), interpolation='nearest')
-	plt.show()
+	# plt.imshow(np.reshape(w, (28, 28)), interpolation='nearest')
+	# plt.show()
+	# acc = score(w, test_data, test_labels)
+	# print("accuracy of classifier trained on the entire training data {}".format(acc))
+
+	plot_misclassified_test(w)
--- ex3/skeleton_sgd.py
+++ ex3/skeleton_sgd.py
@@ -8,6 +8,11 @@ import numpy as np
 import numpy.random
 from sklearn.datasets import fetch_mldata
 import sklearn.preprocessing
+import matplotlib.pyplot as plt
+import math
+
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
 
 """
 Assignment 3 question 2 skeleton.
@@ -16,7 +21,7 @@ Please use the provided function signature for the SGD implementation.
 Feel free to add functions and other code, and submit this file with the name sgd.py
 """
 
-def helper():
+def helper_unscale():
   mnist = fetch_mldata('MNIST original', data_home='.')
   data = mnist['data']
   labels = mnist['target']
@@ -26,7 +31,7 @@ def helper():
   train_idx = numpy.random.RandomState(0).permutation(np.where((labels[:60000] == neg) | (labels[:60000] == pos))[0])
   test_idx = numpy.random.RandomState(0).permutation(np.where((labels[60000:] == neg) | (labels[60000:] == pos))[0])
 
-  print("{} {}".format(train_idx.shape, test_idx.shape))
+  # print("{} {}".format(train_idx.shape, test_idx.shape))
 
   train_data_unscaled = data[train_idx[:6000], :].astype(float)
   train_labels = (labels[train_idx[:6000]] == pos)*2-1 # zero is mapped to -1 , 8 to +1
@@ -36,14 +41,24 @@ def helper():
 
   test_data_unscaled = data[60000+test_idx, :].astype(float)
   test_labels = (labels[60000+test_idx] == pos)*2-1
+ 
+  print("train size {} validation size {} test size {}".\
+    format(len(train_labels), len(validation_labels), len(test_labels)))
 
-  # Preprocessing
-  train_data = sklearn.preprocessing.scale(train_data_unscaled, axis=0, with_std=False)
-  validation_data = sklearn.preprocessing.scale(validation_data_unscaled, axis=0, with_std=False)
-  test_data = sklearn.preprocessing.scale(test_data_unscaled, axis=0, with_std=False)
-  return train_data, train_labels, validation_data, validation_labels, test_data, test_labels
+  return train_data_unscaled, train_labels, validation_data_unscaled, \
+    validation_labels, test_data_unscaled, test_labels
 
-def SGD(data, labels, C=10000, eta_0=0.01, T=10):
+def helper():
+    train_data_unscaled, train_labels, validation_data_unscaled, \
+    validation_labels, test_data_unscaled, test_labels = helper_unscale()
+
+    train_data = sklearn.preprocessing.scale(train_data_unscaled, axis=0, with_std=False)
+    validation_data = sklearn.preprocessing.scale(validation_data_unscaled, axis=0, with_std=False)
+    test_data = sklearn.preprocessing.scale(test_data_unscaled, axis=0, with_std=False)
+    return train_data, train_labels, validation_data, validation_labels, test_data, test_labels
+
+
+def SGD(data, labels, C, eta_0, T):
   """
   Implements Hinge loss using SGD.
   returns: nd array of shape (data.shape[1],) or (data.shape[1],1) representing the final classifier
@@ -51,19 +66,68 @@ def SGD(data, labels, C=10000, eta_0=0.01, T=10):
   d = data.shape[1]
   n = data.shape[0]
   w = np.zeros(d)
-
+ 
   for t in range(T):
-    idx = np.random.RandomState(t).permutation(n)
-    for i in idx:
-      if 1 - labels[i] * np.dot(w, data[i, :]) < 0:
-        w = (1 - eta_0) * w
-      else:
-        w = (1 - eta_0) * w + C * eta_0 * labels[i] * data[i, :]
-
-    return w
+    eta = eta_0 / (t + 1)
+    i = np.random.choice(n)
+    if labels[i] * np.dot(w, data[i, :]) < 1: 
+      w = (1 - eta) * w + C * eta * labels[i] * data[i, :]
+    else:
+      w = (1 - eta) * w
+  return w
 	
 def score(w, X, y):
   return 1 - (np.sum(np.sign((np.matmul(X, w.T) * 2 - 1)) != y)) / X.shape[0]
+
+def iterate_eta_0():
+  vals = [math.pow(10, i) for i in range(-5, 6)]
+  n = len(validation_labels)
+  acc_vals = []
+
+  for eta_0 in vals:
+    acc = 0
+    for i in range(10):
+      w = SGD(train_data, train_labels, 1, eta_0, 1000)
+      acc += score(w, validation_data, validation_labels)
+    acc_vals.append(acc / 10)
+
+  title = 'acc_vs_eta_0'
+  plt.plot(np.log10(vals), acc_vals, 'g-', linewidth=2)
+  plt.xlabel('log10(eta_0)')
+  plt.ylabel('Accuracy')
+  plt.title(title)
+  plt.xticks(np.arange(-5, 6))
+  plt.savefig(title)
+  # plt.show()
+  plt.close()
+
+  best_eta_0 = math.pow(10,-5) * math.pow(10, np.argmax(acc_vals))
+  print("Best eta_0 {}".format(best_eta_0))
+
+def iterate_C():
+  vals = [math.pow(10, i) for i in range(-5, 6)]
+  n = len(validation_labels)
+  acc_vals = []
+
+  for C in vals:
+    acc = 0
+    for i in range(10):
+      w = SGD(train_data, train_labels, C, 1, 1000)
+      acc += score(w, validation_data, validation_labels)
+    acc_vals.append(acc / 10)
+
+  title = 'acc_vs_C'
+  plt.plot(np.log10(vals), acc_vals, 'b-', linewidth=2)
+  plt.xlabel('log10(C)')
+  plt.ylabel('Accuracy')
+  plt.title(title)
+  plt.xticks(np.arange(-5, 6))
+  plt.savefig(title)
+  # plt.show()
+  plt.close()
+
+  best_C = math.pow(10,-5) * math.pow(10, np.argmax(acc_vals))
+  print("Best C {}".format(best_C))
 
 #################################
 
@@ -73,13 +137,11 @@ def score(w, X, y):
 
 if __name__ == '__main__':
   train_data, train_labels, validation_data, validation_labels, test_data, test_labels = helper()
-  w = SGD(train_data, train_labels)
+  # iterate_eta_0()
+  # iterate_C()
 
-  acc = score(w, train_data, train_labels)
-  print("accuracy train {}".format(acc))
-
-  acc = score(w, validation_data, validation_labels)
-  print("accuracy validation {}".format(acc))
-
+  w = SGD(train_data, train_labels, 0.0001, 1, 20000)
+  # plt.imshow(np.reshape(w, (28, 28)), interpolation='nearest')
+  # plt.show()
   acc = score(w, test_data, test_labels)
-  print("accuracy test {}".format(acc))
+  print("SGD accuracy on test data {}".format(acc))
